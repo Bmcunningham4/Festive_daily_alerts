ded5702f0f4a48e57dbfc5bc28022254a05ca587
Ben Cunningham <bmcunningham4@outlook.com>
2024-01-02 19:20:45 +0800

    Need to find a better website than this, good start on data..

---
+++ data.py
@@ -0,0 +1,62 @@
+
+#? Making this so I can store my data that I'm slowly gonna add
+# Remember to go to the old chatgpt page and ask it some questions about the website! Like use on phone ...
+
+
+#* Unofficial holidays / observance days
+
+
+#? remember they've got to be in this form:
+"""
+const holidays = {
+    '01-01': 'Happy New Year!',
+    '02-14': 'Happy Valentine\'s Day!',
+    '07-04': 'Happy Independence Day!',
+    '12-20': "Ben's day get around him!",
+    // Add more holidays as needed with the format 'MM-DD': 'Holiday Greeting'
+    '12-21': 'Happy Winter Solstice!',
+    '12-22': 'Happy Hanukkah!',
+    '12-23': 'Happy Festivus!',
+  };
+"""
+
+#? Website 1: Potentially India -  https://www.jagranjosh.com/general-knowledge/important-days-of-january-1545384820-1
+"""1 January 2024 - Global Family Day
+
+2 January 2024 - World Introvert Day
+
+3 January 2024 - International Mind Body Wellness Day
+
+4 January 2024 - World Braille Day
+
+6 January 2024 - World Day of War Orphans
+
+8 January 2024 - African National Congress Foundation Day
+
+9 January 2024 - NRI (Non-Resident Indian) Day or Pravasi Bharatiya Divas
+
+11 January 2024 - Death anniversary of Lal Bahadur Shastri
+
+12 January 2024 - National Youth Day
+
+13 January 2024 - Lohri
+
+15 January 2024 - Indian Army Day
+
+23 January 2024 - Netaji Subhas Chandra Bose Jayanti
+
+24 January 2024 - National Girl Child Day
+
+25 January 2024 - National Voters Day
+
+25 January 2024 - National Tourism Day
+
+26 January 2024 - Republic Day
+
+26 January 2024 - International Customs Day
+
+28 January 2024 - Birth Anniversary of Lala Lajpat Rai
+
+30 January 2024 - Martyrs Day or Shaheed Diwas
+
+30 January 2024 - World Leprosy Day (Last Sunday of January)"""
